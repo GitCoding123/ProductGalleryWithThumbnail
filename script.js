e2aaa8def77c4244dc3f80a1e388bc698076dd59
Brian Gerkens <67236902+GitCoding123@users.noreply.github.com>
2024-02-07 20:15:39 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const imgLarge = document.getElementById("large-img");
+const productImages = document.querySelectorAll(".img-small img");
+
+console.log(productImages.length);
+
+productImages.forEach((image) => {
+    image.addEventListener("click", (e) => {
+        let src = e.target.getAttribute("src");
+        imgLarge.src = src;
+    });
+});
+
